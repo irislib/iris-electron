fe585e380810c52f8d02c4025e8b225e6c37cdad
Martti Malmi <sirius@iki.fi>
2019-09-30 14:34:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# iris-electron
+
+[Iris-angular](https://github.com/irislib/iris-angular) Electron app
+
+## develop
+git submodule init
+yarn
+yarn start
+
+## build
+yarn pack
+
+## build distribution
+yarn dist
---
+++ index.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+const Gun = require("gun");
+require("gun/lib/multicast");
+const server = require('http').createServer(Gun.serve(__dirname + "iris-angular/dist"));
+const gun = Gun({web: server.listen(8765), multicast: { port: 8765 } });
+console.log('Relay peer started on port ' + 8765 + ' with /gun');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {nodeIntegration: true},
+    icon: path.join(__dirname, 'iris-angular/dist/assets/images/icon128.png')
+  });
+
+  // load the dist folder from Angular
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `iris-angular/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // The following is optional and will open the DevTools:
+  if (process.env.DEV) {
+    win.webContents.openDevTools();
+  }
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
