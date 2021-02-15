ae72ed226fb6699609e6f38afb8b7be833fe6e8e
Martti Malmi <sirius@iki.fi>
2021-02-15 21:48:56 +0200

    v1.6.0

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function createGun() {
 					win.minimize();
 					break;
 				case 'maximize':
-					win.isMaximized() ? win.restore() : win.maximize();
+					win.isMaximized() ? win.unmaximize() : win.maximize();
 					break;
 				case 'close':
 					win.close();
