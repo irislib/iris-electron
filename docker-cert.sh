dfc0083804ae4f4f4749de494c914e3fdf9af5e2
Martti Malmi <sirius@iki.fi>
2021-08-20 16:08:29 +0300

    v2.1.0

--- docker-cert.sh
+++ docker-cert.sh
@@ -8,4 +8,4 @@ docker run --rm -ti \
  -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
- electronuserland/builder:wine
+  electronuserland/builder:12-11.19
