47b73003c7e38d105cc3be0354a1c79da39dcd0c
Martti Malmi <sirius@iki.fi>
2022-06-27 11:48:24 +0300

    v2.3.3

--- docker-cert.sh
+++ docker-cert.sh
@@ -8,4 +8,4 @@ docker run --rm -ti \
  -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-  electronuserland/builder:12-11.19
+  electronuserland/builder:wine
